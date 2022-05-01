8924e575d52ad62635c38713f0e4be85c3908f55
htenezaca <h.tenezaca@uniandes.edu.co>
2022-05-01 08:35:13 -0500

    Added tests for the components and service

--- src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.spec.ts
+++ src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.spec.ts
@@ -2,16 +2,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import faker from '@faker-js/faker';
 
+import { Vehiculo } from '../vehiculo';
 import { ListarVehiculoComponent } from './listar-vehiculo.component';
 
 describe('ListarVehiculoComponent', () => {
   let component: ListarVehiculoComponent;
   let fixture: ComponentFixture<ListarVehiculoComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListarVehiculoComponent ]
+      declarations: [ListarVehiculoComponent],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   }));
@@ -19,10 +24,32 @@ describe('ListarVehiculoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarVehiculoComponent);
     component = fixture.componentInstance;
+    let vehiculos: Vehiculo[] = [];
+
+    for (let index = 1; index <= 3; index++) {
+      vehiculos.push(new Vehiculo(
+        index,
+        faker.vehicle.manufacturer(),
+        faker.vehicle.model(),
+        faker.vehicle.type(),
+        faker.random.number({ min: 1970, max: 2022 }),
+        faker.random.number({ min: 0, max: 200000 }),
+        faker.commerce.color(),
+        faker.image.imageUrl()
+      ));
+    }
+
+    component.vehiculos = vehiculos;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should has correct number of rows (3 rows of vehicles and header) ', () => {
+    fixture.detectChanges();
+    const rowDebugElements = debug.queryAll(By.css('table tr'));
+    expect(rowDebugElements.length).toBe(4);
   });
 });
--- src/app/vehiculo/vehiculo.service.spec.ts
+++ src/app/vehiculo/vehiculo.service.spec.ts
@@ -1,12 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { VehiculoService } from './vehiculo.service';
 
 describe('Service: Vehiculo', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [VehiculoService]
+      providers: [VehiculoService],
+      imports: [HttpClientTestingModule],
     });
   });
 
